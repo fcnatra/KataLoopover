dcfe76419e90e822e81366c2828b6291bd1e1917
Frank Torres <frank.torres@retabet.es>
2024-11-22 11:57:13 +0100

    test when it's needed to switch 3 times to the left

--- LooperTests/LoopoverTests.cs
+++ LooperTests/LoopoverTests.cs
@@ -63,6 +63,24 @@ public class LoopoverTests
         Assert.Equal(expectedMoves, moves);
         Assert.Equal(solvedBoard, Loopover.LastBoardSolved);
     }
+ 
+    [Fact]
+    public void WhenOneCharIsOffToTheRightBy3Cols_Moves3Right()
+    {
+        char[][] solvedBoard = [
+            ['a','b','c','d'],
+            ['e','f','g','h']];
+        char[][] mixedUpBoard = [
+            ['a','b','c','d'],
+            ['f','g','h','e']];
+
+        List<string> expectedMoves = ["L1", "L1", "L1"];
+
+        List<string>? moves = Loopover.Solve(mixedUpBoard, solvedBoard);
+        
+        Assert.Equal(expectedMoves, moves);
+        Assert.Equal(solvedBoard, Loopover.LastBoardSolved);
+    }
 
     [Fact]
     public void WhenOneCharIsOffToTheRightOnEachRow_MovesToTheLeftOnEachRow()
