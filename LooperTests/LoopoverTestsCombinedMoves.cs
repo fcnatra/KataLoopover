d329a299d52135b571f8a4c964b3c0e991016a73
Frank Torres <frank.torres@retabet.es>
2024-11-22 12:56:35 +0100

    WIP testing combined movements

---
+++ LooperTests/LoopoverTestsCombinedMoves.cs
@@ -0,0 +1,22 @@
+namespace LooperTests;
+
+public class LoopoverTestsCombinedMoves
+{
+    [Fact]
+    public void WhenBoardsContainsDiffChars_ReturnsNull()
+    {
+        char[][] solvedBoard = [
+            ['a','b'],
+            ['c','d']];
+        char[][] mixedUpBoard = [
+            ['c','b'],
+            ['d','a']];
+
+        List<string> expectedMoves = ["L1", "U1"];
+
+        List<string>? moves = Loopover.Solve(mixedUpBoard, solvedBoard);
+        
+        Assert.Equal(expectedMoves, moves);
+        Assert.Equal(solvedBoard, Loopover.LastBoardSolved);
+    }
+}
